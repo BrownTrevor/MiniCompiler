4f327170bdd36d482919a0184183250282ed00e0
Thomas Fahrner <33201730+tfahrn@users.noreply.github.com>
2019-06-02 18:33:26 -0700

    script now generates exec and diffs output; m2 finished

--- test_mini.py
+++ test_mini.py
@@ -9,9 +9,17 @@ for benchmark in os.listdir(directory):
    for filename in os.listdir(path):
       if filename.endswith(".mini"):
          mini_path = os.path.join(path, filename)
+         exec_path = os.path.join(path, "exec")
+         input_path = os.path.join(path, "input")
+         my_output_path = os.path.join(path, "myoutput")
+         test_output_path = os.path.join(path, "output")
 
          os.system("java MiniCompiler " + mini_path)
-         os.system("clang -m32 " + mini_path + ".out.ll  'utility.c' ")
+         os.system("clang -m32 " + mini_path + ".out.ll  'utility.c' " +
+            "-o " + exec_path)
+         os.system("./" + exec_path + " < " + input_path + " > " + my_output_path)
+         os.system("diff " + my_output_path + " " + test_output_path)
+
          #os.system("diff a.out " + mini_path + ".out.ll")
 
 
